6f40d911ebbd7ff79a41a351f42beeff0f689267
dualcnhq <nhq1829@gmail.com>
2015-07-29 12:05:55 +0800

    Add listeners

--- app/src/main/java/com/svi/activitytracker/ui/SignUpActivity.java
+++ app/src/main/java/com/svi/activitytracker/ui/SignUpActivity.java
@@ -1,11 +1,11 @@
 package com.svi.activitytracker.ui;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.svi.activitytracker.R;
 
@@ -13,37 +13,43 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
 
-    @Bind(R.id.loginEmailText)
+public class SignUpActivity extends AppCompatActivity {
+
+    @Bind(R.id.signUpEmailText)
     EditText emailText;
-    @Bind(R.id.loginPasswordText)
+    @Bind(R.id.signUpPasswordText)
     EditText passwordText;
+    @Bind(R.id.signUpConfirmPasswordText)
+    EditText confirmPasswordText;
 
-    private static final String TAG = "LoginActivity";
+    private static final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
 
         getSupportActionBar().hide();
     }
 
-    @OnClick(R.id.loginButton)
-    public void loginButton(){
+
+    @OnClick(R.id.startButton)
+    public void startButton(){
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
+        String confirmPassword = confirmPasswordText.getText().toString();
 
         Log.d(TAG, "email: " + email + " password: " + password);
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
     }
 
-    @OnClick(R.id.signUpText)
-    public void newTracker(){
-        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+    @OnClick(R.id.alreadyTrackerText)
+    public void alreadyTracker(){
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
     }
+
 }
--- app/src/main/java/com/svi/activitytracker/ui/SplashActivity.java
+++ app/src/main/java/com/svi/activitytracker/ui/SplashActivity.java
@@ -1,22 +1,35 @@
 package com.svi.activitytracker.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.svi.activitytracker.R;
 
-/**
- * Created by dualc on 7/29/15.
- */
-public class SplashActivity extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
 
+        getSupportActionBar().hide();
+    }
 
+    @OnClick(R.id.btnSignUp)
+    public void signUp() {
+        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        finish();
+    }
 
+    @OnClick(R.id.btnLogin)
+    public void login() {
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
     }
 }
