a8fa9b7cca16e76b953eebe0f17b73489aed15c9
katichka87 <katichka87@gmail.com>
2015-08-07 18:41:13 +0300

    Add layout for an activity detail fragment

--- app/src/main/java/com/svi/activitytracker/fragment/ActivityDetailfragment.java
+++ app/src/main/java/com/svi/activitytracker/fragment/ActivityDetailfragment.java
@@ -1,4 +1,23 @@
 package com.svi.activitytracker.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.svi.activitytracker.R;
+
 public class ActivityDetailfragment extends AbsActivityFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_detail_layout, container, false);
+
+
+
+
+        return view;
+    }
 }
