4d140fbc6ca477e3455f452a4c610b5062fc4867
dualcnhq <nhq1829@gmail.com>
2015-08-13 13:38:20 +0800

    Add saving manage activities state in preferences

--- app/src/main/java/com/svi/activitytracker/ui/ManageActivity.java
+++ app/src/main/java/com/svi/activitytracker/ui/ManageActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.svi.activitytracker.R;
+import com.svi.activitytracker.utils.ActivityUtils;
 import com.svi.activitytracker.view.CustomImageView;
 
 import butterknife.Bind;
@@ -63,14 +64,53 @@ public class ManageActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void setImageResources(){
-        activityWalking.setImageResource(R.drawable.walking);
-        activityDriving.setImageResource(R.drawable.driving_a_car_not_selected);
-        activityRidingBus.setImageResource(R.drawable.riding_a_bus_not_selected);
-        activityCycling.setImageResource(R.drawable.cycling_not_selected);
-        activityRunning.setImageResource(R.drawable.running_not_selected);
-        activitySwimming.setImageResource(R.drawable.swimming_not_selected);
-        activityBasketball.setImageResource(R.drawable.basketball_not_selected);
-        activityRidingTrain.setImageResource(R.drawable.riding_a_train_not_selected);
+        if(ActivityUtils.getIsWalking(getApplicationContext())){
+            activityWalking.setImageResource(R.drawable.walking);
+        } else{
+            activityWalking.setImageResource(R.drawable.walking_not_selected);
+        }
+
+        if(ActivityUtils.getIsDriving(getApplicationContext())){
+            activityDriving.setImageResource(R.drawable.driving_a_car);
+        } else {
+            activityDriving.setImageResource(R.drawable.driving_a_car_not_selected);
+        }
+
+        if(ActivityUtils.getIsRidingBus(getApplicationContext())){
+            activityRidingBus.setImageResource(R.drawable.riding_a_bus);
+        } else {
+            activityRidingBus.setImageResource(R.drawable.riding_a_bus_not_selected);
+        }
+
+        if(ActivityUtils.getIsCycling(getApplicationContext())){
+            activityCycling.setImageResource(R.drawable.cycling);
+        } else {
+            activityCycling.setImageResource(R.drawable.cycling_not_selected);
+        }
+
+        if(ActivityUtils.getIsRunning(getApplicationContext())) {
+            activityRunning.setImageResource(R.drawable.running);
+        } else {
+            activityRunning.setImageResource(R.drawable.running_not_selected);
+        }
+
+        if(ActivityUtils.getIsSwimming(getApplicationContext())){
+            activitySwimming.setImageResource(R.drawable.swimming);
+        } else {
+            activitySwimming.setImageResource(R.drawable.swimming_not_selected);
+        }
+
+        if(ActivityUtils.getIsBasketball(getApplicationContext())){
+            activityBasketball.setImageResource(R.drawable.basketball);
+        } else {
+            activityBasketball.setImageResource(R.drawable.basketball_not_selected);
+        }
+
+        if(ActivityUtils.getIsRidingTrain(getApplicationContext())) {
+            activityRidingTrain.setImageResource(R.drawable.riding_a_train);
+        } else {
+            activityRidingTrain.setImageResource(R.drawable.riding_a_train_not_selected);
+        }
     }
 
 
@@ -82,64 +122,80 @@ public class ManageActivity extends AppCompatActivity implements View.OnClickLis
                 drawable = R.drawable.walking;
                 if(activityWalking.getResourceId() == drawable){ // deactivate walking
                     activityWalking.setImageResource(R.drawable.walking_not_selected);
+                    ActivityUtils.setIsWalking(getApplicationContext(), false);
                 } else { // activate walking
                     activityWalking.setImageResource(R.drawable.walking);
+                    ActivityUtils.setIsWalking(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityDriving:
                 drawable = R.drawable.driving_a_car;
                 if(activityDriving.getResourceId() == drawable){ // deactivate driving
                     activityDriving.setImageResource(R.drawable.driving_a_car_not_selected);
+                    ActivityUtils.setIsDriving(getApplicationContext(), false);
                 } else { // activate driving
                     activityDriving.setImageResource(R.drawable.driving_a_car);
+                    ActivityUtils.setIsDriving(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityRidingBus:
                 drawable = R.drawable.riding_a_bus;
                 if(activityRidingBus.getResourceId() == drawable){ // deactivate riding bus
                     activityRidingBus.setImageResource(R.drawable.riding_a_bus_not_selected);
+                    ActivityUtils.setIsRidingBus(getApplicationContext(), false);
                 } else { // activate riding bus
                     activityRidingBus.setImageResource(R.drawable.riding_a_bus);
+                    ActivityUtils.setIsRidingBus(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityCycling:
                 drawable = R.drawable.cycling;
                 if(activityCycling.getResourceId() == drawable){ // deactivate cycling
                     activityCycling.setImageResource(R.drawable.cycling_not_selected);
+                    ActivityUtils.setIsCycling(getApplicationContext(), false);
                 } else { // activate cycling
                     activityCycling.setImageResource(R.drawable.cycling);
+                    ActivityUtils.setIsCycling(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityRunning:
                 drawable = R.drawable.running;
                 if(activityRunning.getResourceId() == drawable){ // deactivate running
                     activityRunning.setImageResource(R.drawable.running_not_selected);
+                    ActivityUtils.setIsRunning(getApplicationContext(), false);
                 } else { // activate running
                     activityRunning.setImageResource(R.drawable.running);
+                    ActivityUtils.setIsRunning(getApplicationContext(), true);
                 }
                 break;
             case R.id.activitySwimming:
                 drawable = R.drawable.swimming;
                 if(activitySwimming.getResourceId() == drawable){ // deactivate swimming
                     activitySwimming.setImageResource(R.drawable.swimming_not_selected);
+                    ActivityUtils.setIsSwimming(getApplicationContext(), false);
                 } else { // activate swimming
                     activitySwimming.setImageResource(R.drawable.swimming);
+                    ActivityUtils.setIsSwimming(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityBasketball:
                 drawable = R.drawable.basketball;
                 if(activityBasketball.getResourceId() == drawable){ // deactivate basketball
                     activityBasketball.setImageResource(R.drawable.basketball_not_selected);
+                    ActivityUtils.setIsBasketball(getApplicationContext(), false);
                 } else { // activate basketball
                     activityBasketball.setImageResource(R.drawable.basketball);
+                    ActivityUtils.setIsBasketball(getApplicationContext(), true);
                 }
                 break;
             case R.id.activityRidingTrain:
                 drawable = R.drawable.riding_a_train;
                 if(activityRidingTrain.getResourceId() == drawable){ // deactivate riding train
                     activityRidingTrain.setImageResource(R.drawable.riding_a_train_not_selected);
+                    ActivityUtils.setIsRidingTrain(getApplicationContext(), false);
                 } else { // activate riding train
                     activityRidingTrain.setImageResource(R.drawable.riding_a_train);
+                    ActivityUtils.setIsRidingTrain(getApplicationContext(), true);
                 }
                 break;
         }
